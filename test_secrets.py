c1f39f465438da1f31696f8263f286f257c771e1
arnaud <arnaudmiribel@gmail.com>
2021-10-20 18:21:39 +0200

    More sizes

--- test_secrets.py
+++ test_secrets.py
@@ -6,5 +6,6 @@ st.write("---")
 
 with st.echo():
     st.image("big-query-3.png", None, 300)
-
+    st.image("big-query-3.png", None, 600)
+    st.image("big-query-3.png", None, 900)
     st.image("big-query-3.png")
