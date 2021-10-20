c26042c8794fe76f6745e41da9dfd82090e975e8
arnaud <arnaudmiribel@gmail.com>
2021-10-20 18:22:54 +0200

    More sizes and cols

--- test_secrets.py
+++ test_secrets.py
@@ -9,3 +9,9 @@ with st.echo():
     st.image("big-query-3.png", None, 600)
     st.image("big-query-3.png", None, 900)
     st.image("big-query-3.png")
+    with st.columns(2)[0]:
+        st.image("big-query-3.png")
+    with st.columns(3)[0]:
+        st.image("big-query-3.png")
+    with st.columns(4)[0]:
+        st.image("big-query-3.png")
