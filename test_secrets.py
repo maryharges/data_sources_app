ac633db70b3ded283c3bc470db2ce6b5679eeac6
arnaud <arnaudmiribel@gmail.com>
2021-10-20 18:19:31 +0200

    Change param

--- test_secrets.py
+++ test_secrets.py
@@ -5,6 +5,6 @@ st.write(f"You filled in the following secrets: {st.secrets}")
 st.write("---")
 
 with st.echo():
-    st.image("big-query-3.png", 300)
+    st.image("big-query-3.png", None, 300)
 
     st.image("big-query-3.png")
