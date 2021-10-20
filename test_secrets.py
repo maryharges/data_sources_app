3b07d6b0b1100fafcdebeed6a7a3d956d5453dcd
arnaud <arnaudmiribel@gmail.com>
2021-10-20 18:18:54 +0200

    Test image

--- test_secrets.py
+++ test_secrets.py
@@ -2,5 +2,9 @@ import streamlit as st
 
 st.write(f"You filled in the following secrets: {st.secrets}")
 
-st.write("---"
-)
+st.write("---")
+
+with st.echo():
+    st.image("big-query-3.png", 300)
+
+    st.image("big-query-3.png")
